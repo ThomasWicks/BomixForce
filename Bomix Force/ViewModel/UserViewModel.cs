2b501610c8e0955cc6e0732482a90cbc2e356476
BrunoCortesGuerra <bc.guerra1000@gmail.com>
2020-08-02 14:37:46 -0300

    Layout do usuario Iniciado.

---
+++ Bomix Force/ViewModel/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bomix_Force.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+    }
+}
