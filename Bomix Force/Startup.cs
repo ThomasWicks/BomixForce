ec999f898577e997478e31ca060458d7ca0a5e64
Thomas Wicks <thomas.wicks@hotmail.com>
2020-08-19 20:26:49 -0300

    Initial roles config

--- Bomix Force/Startup.cs
+++ Bomix Force/Startup.cs
@@ -13,6 +13,8 @@ using AutoMapper;
 using Bomix_Force.Util;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+using System;
 
 namespace Bomix_Force
 {
@@ -35,23 +37,25 @@ namespace Bomix_Force
                 options.UseSqlServer(
                     Configuration.GetConnectionString("Mysqlconnection")));
 
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("Delete", policy => policy.Requirements.Add(new AuthorizationHelper("Delete")));
-                options.AddPolicy("Create", policy => policy.Requirements.Add(new AuthorizationHelper("Create")));
-                options.AddPolicy("Edit", policy => policy.Requirements.Add(new AuthorizationHelper("Edit")));
-                options.AddPolicy("Index", policy => policy.Requirements.Add(new AuthorizationHelper("Index")));
-            }
-            );
-
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ModelContext>();
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("RequireAdministratorRole",
+                     policy => policy.RequireRole("Admin"));
+                options.AddPolicy("RequireCompanyRole",
+                     policy => policy.RequireRole("Company"));
+                options.AddPolicy("RequirUserRole",
+                    policy => policy.RequireRole("User"));
+            });
+
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddSingleton<IAuthorizationHandler, AuthHendler>();
             // Auto Mapper Configurations
-            services.AddAutoMapper(typeof (Startup));
+            services.AddAutoMapper(typeof(Startup));
 
             //Injeção de dependencia
             services.AddScoped<ModelContext>();
@@ -62,7 +66,59 @@ namespace Bomix_Force
             //services.AddScoped<IGenericRepository<Permission>, GenericRepository<Permission>>();
             services.AddScoped<IGenericRepository<Person>, GenericRepository<Person>>();
             //services.AddScoped<IGenericRepository<Bomix_Force.Data.Entities.Profile>, GenericRepository<Bomix_Force.Data.Entities.Profile>>();
-            //services.AddScoped<IGenericRepository<UserLogin>, GenericRepository<UserLogin>>();
+
+            createRolesandUsers(createProvider(services)).Wait();
+        }
+        public IServiceProvider createProvider(IServiceCollection services)
+        {
+            return services.BuildServiceProvider();
+        }
+
+        private async Task createRolesandUsers(IServiceProvider serviceProvider)
+        {
+            var _roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var _userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            bool x = await _roleManager.RoleExistsAsync("Admin");
+            if (!x)
+            {
+                // first we create Admin rool    
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                await _roleManager.CreateAsync(role);
+
+                //Here we create a Admin super user who will maintain the website                   
+
+                var user = new IdentityUser { UserName = "thomas.wicks@hotmail.com", Email = "thomas.wicks@hotmail.com" };
+
+
+                string userPWD = "Admin123@";
+
+                IdentityResult chkUser = await _userManager.CreateAsync(user, userPWD);
+
+                //Add default User to Role Admin    
+                if (chkUser.Succeeded)
+                {
+                    var result1 = await _userManager.AddToRoleAsync(user, "Admin");
+                }
+            }
+
+            // creating Creating Manager role     
+            x = await _roleManager.RoleExistsAsync("Manager");
+            if (!x)
+            {
+                var role = new IdentityRole();
+                role.Name = "Manager";
+                await _roleManager.CreateAsync(role);
+            }
+
+            // creating Creating Employee role     
+            x = await _roleManager.RoleExistsAsync("User");
+            if (!x)
+            {
+                var role = new IdentityRole();
+                role.Name = "User";
+                await _roleManager.CreateAsync(role);
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
