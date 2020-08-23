b0fb6029cac833d87b124f8a2bf6f90d32c9c535
BrunoCortesGuerra <bc.guerra1000@gmail.com>
2020-08-23 14:59:36 -0300

    modal index ajustes

--- Bomix Force/Controllers/UserController.cs
+++ Bomix Force/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
+using Bomix_Force.Areas.Identity.Pages.Account;
 using Bomix_Force.Data.Entities;
 using Bomix_Force.Repo.Interface;
 using Bomix_Force.ViewModels;
@@ -11,20 +12,28 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Bomix_Force.Controllers
 {
     [Authorize]
     public class UserController : Controller
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
         private readonly IGenericRepository<Company> _genericCompanyService;
         private readonly IGenericRepository<Person> _genericPersonService;
         private readonly IMapper _mapper;
-        public UserController(IGenericRepository<Person> genericPersonService, IGenericRepository<Company> genericCompanyService, IMapper mapper)
+        public UserController(IGenericRepository<Person> genericPersonService, IGenericRepository<Company> genericCompanyService,
+        IMapper mapper, SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, ILogger<RegisterModel> logger)
         {
             _genericPersonService = genericPersonService;
             _mapper = mapper;
             _genericCompanyService = genericCompanyService;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _logger = logger;
 
         }
         [Authorize]
@@ -34,14 +43,17 @@ namespace Bomix_Force.Controllers
             var teste = User.IsInRole("Company");
             if (User.IsInRole("Admin"))
             {
-                IEnumerable<UserViewModel> userView = _mapper.Map<IEnumerable<UserViewModel>>(_genericPersonService.Get());
-                foreach (var item in userView)
+                UserViewIndex userList = new UserViewIndex();
+                userList.UserList = new List<UserViewModel>();
+                List<Person> people = _genericPersonService.GetAll().ToList();
+                userList.UserList = _mapper.Map<IEnumerable<UserViewModel>> (people).ToList();
+                foreach (var item in userList.UserList)
                 {
                     Person person = _genericPersonService.Get(u => u.Name == item.Name).First();
-                    item.Company = _genericCompanyService.Get(g => g.Id == person.CompanyId).First(); ;
+                    //item.Input.Company = _genericCompanyService.Get(g => g.Id == person.CompanyId).First(); ;
                 }
 
-                return View(userView);
+                return View(userList.UserList);
             }
             else if (User.IsInRole("Company"))
             {
@@ -52,7 +64,7 @@ namespace Bomix_Force.Controllers
                 IEnumerable<UserViewModel> userView = _mapper.Map<IEnumerable<UserViewModel>>(_genericPersonService.Get(g => g.CompanyId == person.CompanyId));
                 foreach (var item in userView)
                 {
-                    item.Company = company;
+                    item.CompanyName = company.Name;
                 }
 
                 return View(userView);
@@ -69,16 +81,46 @@ namespace Bomix_Force.Controllers
         // GET: UserController/Create
         public ActionResult Create()
         {
+            List<Company> company = new List<Company>();
+            company = _genericCompanyService.GetAll().ToList();
+            //ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             return View();
         }
 
         // POST: UserController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<ActionResult> Create(UserViewModel userviewModel)
         {
             try
             {
+                //ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+                if (ModelState.IsValid)
+                {
+
+                    //TODO TEST IF COMPANY QUERY WORKS
+                    Company company = _genericCompanyService.Get(c => c.Name == userviewModel.CompanyName).First();
+                    var user = new IdentityUser { UserName = userviewModel.UserName, Email = userviewModel.Email };
+                    var result = await _userManager.CreateAsync(user, userviewModel.Password);
+
+                    if (result.Succeeded)
+                    {
+                        Person person = new Person { Name = userviewModel.Name, Email = userviewModel.Email, Tel = userviewModel.Tel, CompanyId = company.Id, UserId = user.Id };
+                        _genericPersonService.Insert(person);
+                        _genericPersonService.Save();
+                        _logger.LogInformation("Person = " + person.Tel);
+                        _logger.LogInformation("Novo usuário criado.");
+
+
+                        return RedirectToAction(nameof(Index));
+                    }
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+
+                // If we got this far, something failed, redisplay form
                 return RedirectToAction(nameof(Index));
             }
             catch
--- Bomix Force/ViewModels/UserViewModel.cs
+++ Bomix Force/ViewModels/UserViewModel.cs
@@ -1,17 +1,60 @@
-﻿using Bomix_Force.Data.Entities;
+﻿using Bomix_Force.Areas.Identity.Pages.Account;
+using Bomix_Force.Data.Context;
+using Bomix_Force.Data.Entities;
+using Bomix_Force.Repo.Interface;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Bomix_Force.ViewModels
 {
+ 
     public class UserViewModel
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public int Tel { get; set; }
-        public  Company Company { get; set; }
+            public Guid Id { get; set; }
+            [Required]
+            [Display(Name = "Usuário")]
+            public string UserName { get; set; }
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "A {0} precisa ser pelo menos {2} e no máximo {1} caracteres.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Senha")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirmar senha")]
+            [Compare("Password", ErrorMessage = "As senhas não correspondem.")]
+            public string ConfirmPassword { get; set; }
+
+            [Required]
+            [Display(Name = "Nome da Empresa")]
+            public string CompanyName { get; set; }
+            [Required]
+            public string Name { get; set; }
+            [Required]
+            [Display(Name = "Telefone")]
+            [DataType(DataType.PhoneNumber)]
+            public int Tel { get; set; }
+            [Required]
+            public string Endereço { get; set; }
+        }
+    public class UserViewIndex
+    {
+        public IEnumerable<UserViewModel> UserList { get; set; }
+        public UserViewModel User { get; set; }
     }
+    
 }
