e32371df4c0045bfb88f7621cfb3ef4e77b98539
BrunoCortesGuerra <bc.guerra1000@gmail.com>
2020-08-25 22:16:28 -0300

    Layout index pedidos.

--- Bomix Force/ViewModels/OrderViewModel.cs
+++ Bomix Force/ViewModels/OrderViewModel.cs
@@ -9,9 +9,11 @@ namespace Bomix_Force.ViewModels
     public class OrderViewModel
     {
         public int Id { get; set; }
-        public int Number { get; set; }
-        public DateTime Date { get; set; }
-        public string Status_Order { get; set; }
-        public List<Item> Item { get; set; }
+        public int NumeroPedido { get; set; }
+        public DateTime Emissao { get; set; }
+        public DateTime Entrega { get; set; }
+        public string Status { get; set; }
+        public virtual List<Item> Item { get; set; }
+        public virtual Company Company { get; set; }
     }
 }
