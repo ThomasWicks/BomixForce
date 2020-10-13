b21733c536c84351cd9e3bbb0f7fa4fe6e69a502
Rubem de Almeida <rubemdealmeida@hotmail.com>
2020-10-13 15:00:08 -0300

    Fix: return manage nav bar

--- Bomix Force/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Bomix Force/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -92,7 +92,7 @@ namespace Bomix_Force.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        return LocalRedirect("/Identity/Account/Manage");
+                        return LocalRedirect("/Identity/Account/Manage/ChangePassword");
                     }
                 }
                 if (result.RequiresTwoFactor)
