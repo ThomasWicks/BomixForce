584fde193068a7ed5fa9205cccadbfa3bd7e5299
Thomas Wicks <thomas.wicks@hotmail.com>
2020-12-06 15:56:04 -0300

    Tela de documento e contato

---
+++ Bomix Force/Controllers/DocumentController.cs
@@ -0,0 +1,89 @@
+﻿using Bomix_Force.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bomix_Force.Controllers
+{
+    public class DocumentController : Controller
+    {
+        // GET: DocumentController
+        public ActionResult Index()
+        {
+            List<DocumentViewModel> documentViewModel = new List<DocumentViewModel>();
+            return View(documentViewModel);
+        }
+
+        // GET: DocumentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DocumentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DocumentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DocumentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DocumentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DocumentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DocumentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
